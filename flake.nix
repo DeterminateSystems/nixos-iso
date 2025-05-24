954add6e704a197e74261d4ace13ceb8820e68e5
Graham Christensen <graham@grahamc.com>
2025-05-23 22:20:17 -0400

    fmt, build arm

--- flake.nix
+++ flake.nix
@@ -11,58 +11,105 @@
       fh,
       ...
     }:
+    let
+      forSystems =
+        s: f:
+        nixpkgs.lib.genAttrs s (
+          system:
+          f rec {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+          }
+        );
+
+      forAllSystems = forSystems [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forLinuxSystems = forSystems [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    in
     {
-      packages.x86_64-linux.toplevel = self.nixosConfigurations.install.config.system.build.toplevel;
-      packages.x86_64-linux.iso = self.nixosConfigurations.install.config.system.build.isoImage;
+      devShells = forAllSystems (
+        { system, pkgs, ... }:
+        {
+          default = pkgs.mkShellNoCC {
+            buildInputs = with pkgs; [
+              nixfmt-rfc-style
+            ];
+          };
+        }
+      );
 
-      nixosConfigurations.install = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          # Load the Determinate module
-          determinate.nixosModules.default
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix"
-          (
-            { options, pkgs, lib, ... }:
-            {
-              environment.systemPackages = [ fh.packages.${pkgs.stdenv.hostPlatform.system}.default ];
-              environment.etc."nixos/flake.nix" = {
-                source = ./flake.nix;
-                mode = "0644";
-              };
-              environment.etc."nixos/flake.lock" = {
-                source = ./flake.lock;
-                mode = "0644";
-              };
-              environment.etc."nixos-generate-config.conf".text = ''
-                [Defaults]
-                Flake=1
-              '';
+      packages = forLinuxSystems (
+        { system, ... }:
+        {
+          toplevel = self.nixosConfigurations.${system}.install.config.system.build.toplevel;
+          iso = self.nixosConfigurations.${system}.install.config.system.build.isoImage;
+        }
+      );
 
-              networking.wireless.enable = lib.mkForce false;
-              networking.networkmanager.enable = true;
-
-              system.nixos-generate-config.flake = ''
+      nixosConfigurations = forLinuxSystems (
+        { system, ... }:
+        {
+          install = nixpkgs.lib.nixosSystem {
+            system = system;
+            modules = [
+              # Load the Determinate module
+              determinate.nixosModules.default
+              "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix"
+              (
                 {
-                  inputs = {
-                    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
-                    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # NixOS, rolling release
-                    # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # NixOS, current stable
+                  options,
+                  pkgs,
+                  lib,
+                  ...
+                }:
+                {
+                  environment.systemPackages = [ fh.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+                  environment.etc."nixos/flake.nix" = {
+                    source = ./flake.nix;
+                    mode = "0644";
                   };
-                  outputs = inputs\@{ self, nixpkgs, determinate, ... }: {
-                    # NOTE: '${options.networking.hostName.default}' is the default hostname
-                    nixosConfigurations.${options.networking.hostName.default} = nixpkgs.lib.nixosSystem {
-                      modules = [
-                        determinate.nixosModule.default
-                        ./configuration.nix
-                      ];
-                    };
+                  environment.etc."nixos/flake.lock" = {
+                    source = ./flake.lock;
+                    mode = "0644";
                   };
-                }
-              '';
-            }
-          )
-        ];
-      };
-    };
+                  environment.etc."nixos-generate-config.conf".text = ''
+                    [Defaults]
+                    Flake=1
+                  '';
 
+                  networking.wireless.enable = lib.mkForce false;
+                  networking.networkmanager.enable = true;
+
+                  system.nixos-generate-config.flake = ''
+                    {
+                      inputs = {
+                        determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
+                        nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # NixOS, rolling release
+                        # nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # NixOS, current stable
+                      };
+                      outputs = inputs\@{ self, nixpkgs, determinate, ... }: {
+                        # NOTE: '${options.networking.hostName.default}' is the default hostname
+                        nixosConfigurations.${options.networking.hostName.default} = nixpkgs.lib.nixosSystem {
+                          modules = [
+                            determinate.nixosModule.default
+                            ./configuration.nix
+                          ];
+                        };
+                      };
+                    }
+                  '';
+                }
+              )
+            ];
+          };
+        }
+      );
+    };
 }
